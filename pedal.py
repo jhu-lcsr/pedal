bad1e7eb384e68fe484d316b821e7d7819350175
Jonathan Bohren <jbo@jhu.edu>
2015-07-24 16:25:46 -0400

    because more is more

--- pedal.py
+++ pedal.py
@@ -14,21 +14,25 @@ from evdev import InputDevice, list_devices, categorize, ecodes, events
 class Pedal(object):
     def __init__(self):
 
-        self.pedal_dev = None
-        self.joy_msg = Joy(buttons=[0])
+        self.n_pedals = rospy.get_param('~n_pedals',1)
+        self.pedal_devs = []
+        self.joy_msg = Joy(buttons=[0]*self.n_pedals)
 
-    def update_value(self):
+    def get_value(self, dev):
+        latest_value = None
 
         while not rospy.is_shutdown():
-            event = self.pedal_dev.read_one()
+            event = dev.read_one()
             if event is None:
                 break
             if event.type == ecodes.EV_KEY:
                 print(categorize(event))
                 if event.value in [events.KeyEvent.key_down, events.KeyEvent.key_hold]:
-                    self.joy_msg.buttons[0] = 1
+                    latest_value = 1
                 else:
-                    self.joy_msg.buttons[0] = 0
+                    latest_value = 0
+
+        return latest_value
 
     def spin(self):
         devices = [InputDevice(fn) for fn in list_devices()]
@@ -36,16 +40,26 @@ class Pedal(object):
         for dev in devices:
             if dev.info.vendor == 0x0c45 and dev.info.product == 0x7403:
                 print('Using pedal "'+dev.name+'" on '+dev.phys)
-                self.pedal_dev = dev
-                break
+                print('Info:\n\n{}'.format(dev.info))
 
-        self.pedal_dev.grab()
+                try:
+                   dev.grab()
+                except IOError as err:
+                    print('Couldn\'t grab device, trying again.')
+                    continue
+
+                self.pedal_devs.append(dev)
+                if len(self.pedal_devs) == self.n_pedals:
+                    break
 
         self.joy_pub = rospy.Publisher('joy', Joy)
         r = rospy.Rate(50)
 
         while not rospy.is_shutdown():
-            self.update_value()
+            for i,dev in enumerate(self.pedal_devs):
+                latest_value = self.get_value(dev)
+                if latest_value is not None:
+                    self.joy_msg.buttons[i] = latest_value
             self.joy_msg.header.stamp = rospy.Time.now()
             self.joy_pub.publish(self.joy_msg)
             r.sleep()
